3273f5a21cf430ebbd323643b93fc595f9d43ada
KerttuRungi <kerttu.rungi@gmail.com>
2025-11-12 21:57:59 +0200

    All Spaceships CRUD tests

--- TestingShopSelenium/Test.cs
+++ TestingShopSelenium/Test.cs
@@ -12,9 +12,10 @@ namespace TestingShopSelenium
         public void Initialize()
         {
             driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
         }
 
-        [Test]
+        [Test, Order(1)]
         public void TestSpaceshipAdd()
         {
             driver.Url = "https://localhost:7246/";
@@ -44,6 +45,131 @@ namespace TestingShopSelenium
             Console.WriteLine("Test passed");
         }
 
+        //same data but invalid
+        [Test]
+        public void TestSpaceshipAddInvalidData()
+        {
+            driver.Url = "https://localhost:7246/";
+            IWebElement idOfLinkElement = driver.FindElement(By.Id("spaceship"));
+            idOfLinkElement.Click();
+
+            IWebElement ifOfCreateButton = driver.FindElement(By.Id("TestIdCreate"));
+            ifOfCreateButton.Click();
+
+            Thread.Sleep(500);
+            InsertSpaceshipData(driver);
+
+            IWebElement idOfCrew = driver.FindElement(By.Id("TestIdCrew"));
+            idOfCrew.Clear();
+            idOfCrew.SendKeys("-1234");
+
+            IWebElement ifOfCreateSubmitButton = driver.FindElement(By.Id("TestIdSpaceshipCreate"));
+            ifOfCreateSubmitButton.Click();
+            
+            Console.WriteLine("Invalid data test passed");
+        }
+
+        //details data matches inserted data test
+        [Test, Order(2)]
+        public void TestSpaceshipDetails()
+        {
+
+            driver.Url = "https://localhost:7246/";
+            IWebElement idOfLinkElement = driver.FindElement(By.Id("spaceship"));
+            idOfLinkElement.Click();
+
+            IWebElement ifOfCreateButton = driver.FindElement(By.Id("TestIdDetails"));
+            ifOfCreateButton.Click();
+
+            IWebElement idOfName = driver.FindElement(By.Id("TestIdNameDetails"));
+            Assert.That(idOfName.Text, Is.EqualTo("TestName"));
+
+            IWebElement idOfClassification = driver.FindElement(By.Id("TestIdClassificationDetails"));
+            Assert.That(idOfClassification.Text, Is.EqualTo("TestClassification"));
+
+            IWebElement idOfCrew = driver.FindElement(By.Id("TestIdCrewDetails"));
+            Assert.That(idOfCrew.Text, Is.EqualTo("1234"));
+
+            IWebElement idOfBuildDate = driver.FindElement(By.Id("TestIdBuildDateDetails"));
+            string detailsDate = idOfBuildDate.Text;
+
+            Console.WriteLine("Details test passed");
+        }
+
+        //update data test
+        [Test, Order(3)]
+        public void TestSpaceshipUpdate()
+        {
+            driver.Url = "https://localhost:7246/";
+            IWebElement idOfLinkElement = driver.FindElement(By.Id("spaceship"));
+            idOfLinkElement.Click();
+            IWebElement ifOfUpdateButton = driver.FindElement(By.Id("TestIdUpdate"));
+            ifOfUpdateButton.Click();
+
+            Thread.Sleep(500);
+
+            InsertSpaceshipUpdateData(driver);
+
+            IWebElement ifOfCreateSubmitButton = driver.FindElement(By.Id("TestIdSpaceshipUpdate"));
+            ifOfCreateSubmitButton.Click();
+
+            IWebElement idOfName = driver.FindElement(By.Id("TestIdSpaceshipName"));
+            Assert.That(idOfName.Text, Is.EqualTo("UpdatedTestName"));
+
+            IWebElement idOfClassification = driver.FindElement(By.Id("TestIdSpaceshipClassification"));
+            Assert.That(idOfClassification.Text, Is.EqualTo("UpdatedTestClassification"));
+
+            IWebElement idOfCrew = driver.FindElement(By.Id("TestIdSpaceshipCrew"));
+            Assert.That(idOfCrew.Text, Is.EqualTo("5678"));
+
+           
+
+            Console.WriteLine("Update test passed");
+        }
+
+        //invalid update data test
+        [Test]
+        public void TestSpaceshipUpdateInvalidData()
+        {
+            driver.Url = "https://localhost:7246/";
+            IWebElement idOfLinkElement = driver.FindElement(By.Id("spaceship"));
+            idOfLinkElement.Click();
+
+            IWebElement ifOfUpdateButton = driver.FindElement(By.Id("TestIdUpdate"));
+            ifOfUpdateButton.Click();
+
+            Thread.Sleep(500);
+
+            InsertSpaceshipData(driver);
+
+            IWebElement ifOfCreateSubmitButton = driver.FindElement(By.Id("TestIdSpaceshipUpdate"));
+            ifOfCreateSubmitButton.Click();
+
+            IWebElement idOfCrew = driver.FindElement(By.Id("TestIdCrew"));
+            idOfCrew.Clear();
+            idOfCrew.SendKeys("-1234");
+
+            Console.WriteLine("Invalid data test passed");
+        }
+
+        //delete test
+        [Test, Order(4)]
+        public void TestSpaceshipDelete()
+        {
+            driver.Url = "https://localhost:7246/";
+            IWebElement idOfLinkElement = driver.FindElement(By.Id("spaceship"));
+            idOfLinkElement.Click();
+
+            IWebElement ifOfDeleteButton = driver.FindElement(By.Id("TestIdDelete"));
+            ifOfDeleteButton.Click();
+
+            IWebElement ifOfDeleteSubmitButton = driver.FindElement(By.Id("TestIdSpaceshipDelete"));
+            ifOfDeleteSubmitButton.Click();
+
+            Console.WriteLine("Delete test passed");
+        }
+
+
         private static void InsertSpaceshipData(IWebDriver driver)
         {
             IWebElement idOfName = driver.FindElement(By.Id("TestIdName"));
@@ -64,8 +190,29 @@ namespace TestingShopSelenium
             IWebElement idOfEnginPower = driver.FindElement(By.Id("TestIdEnginPower"));
             idOfEnginPower.SendKeys("4321");
 
+        }
+        private static void InsertSpaceshipUpdateData(IWebDriver driver) {
+            IWebElement idOfName = driver.FindElement(By.Id("TestIdName"));
+            idOfName.Clear();
+            idOfName.SendKeys("UpdatedTestName");
 
+            IWebElement idOfClassification = driver.FindElement(By.Id("TestIdClassification"));
+            idOfClassification.Clear();
 
+            idOfClassification.SendKeys("UpdatedTestClassification");
+            IWebElement idOfDate = driver.FindElement(By.Id("TestIdBuildDate"));
+            DateTime now = DateTime.Now;
+            string formatted = now.ToString("yyyy-MM-ddTHH:mm");
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript($"document.getElementById('TestIdBuildDate').value = '{formatted}';");
+
+            IWebElement idOfCrew = driver.FindElement(By.Id("TestIdCrew"));
+            idOfCrew.Clear();
+            idOfCrew.SendKeys("5678");
+
+            IWebElement idOfEnginPower = driver.FindElement(By.Id("TestIdEnginPower"));
+            idOfEnginPower.Clear();
+            idOfEnginPower.SendKeys("8765");
         }
 
         [TearDown]
